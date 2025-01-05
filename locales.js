57c0b9c3caa479b4a922c7171b05f37bdb89b7bf
yuan <a958787c@qq.com>
2025-01-06 00:15:27 +0800

    feat: 测试contentMenu

---
+++ locales.js
@@ -0,0 +1,14 @@
+export const tldLocales = {
+  'com.au': 'Australia',
+  'com.br': 'Brazil',
+  ca: 'Canada',
+  cn: 'China',
+  fr: 'France',
+  it: 'Italy',
+  'co.in': 'India',
+  'co.jp': 'Japan',
+  'com.ms': 'Mexico',
+  ru: 'Russia',
+  'co.za': 'South Africa',
+  'co.uk': 'United Kingdom'
+};
