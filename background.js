6f805f869a17c9ae6127facbcceb43a9e78a63b6
yuan <a958787c@qq.com>
2025-01-09 18:41:39 +0800

    wip: 暂存

--- background.js
+++ background.js
@@ -1,4 +1,61 @@
 console.log('======== Fetch Video for Douyin background start ========');
+
+// 存储 debugger 连接的标记
+letdebuggerAttached = false;
+
+// 初始化 debugger
+chrome.debugger.onDetach.addListener((source) => {
+  debuggerAttached = false;
+});
+
+// 监听网络请求
+chrome.webRequest.onBeforeRequest.addListener(
+  async function(details) {
+    // 如果还没有附加 debugger
+    if (!debuggerAttached) {
+      try {
+        // 附加 debugger 到当前标签页
+        await chrome.debugger.attach({ tabId: details.tabId }, "1.0");
+        debuggerAttached = true;
+        
+        // 启用网络跟踪
+        await chrome.debugger.sendCommand(
+          { tabId: details.tabId },
+          "Network.enable"
+        );
+      } catch (err) {
+        console.error("Debugger attach failed:", err);
+      }
+    }
+  },
+  { urls: ["<all_urls>"] }
+);
+
+// 监听响应数据
+chrome.debugger.onEvent.addListener((source, method, params) => {
+  if (method === "Network.responseReceived") {
+    const requestId = params.requestId;
+    
+    // 获取响应体
+    chrome.debugger.sendCommand(
+      { tabId: source.tabId },
+      "Network.getResponseBody",
+      { requestId: requestId },
+      (response) => {
+        if (response && response.body) {
+          console.log("Response body:", response.body);
+          // 在这里处理响应内容
+          // 可以发送消息给 content script
+          chrome.tabs.sendMessage(source.tabId, {
+            type: 'response-data',
+            data: response.body
+          });
+        }
+      }
+    );
+  }
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'sidePanel',
@@ -25,23 +82,13 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   }
 });
 
-chrome.declarativeNetRequest.onRequest.addListener(
-  function (request) {
-    console.log('请求详情:', request);
-  },
-  {
-    urls: ["https://*.douyin.com/*"]
-  }
-);
-
 chrome.webRequest.onCompleted.addListener(
-  function (details) {
+  function (...args) {
     console.log('======== webRequest.onCompleted ========');
-    console.log(details);
+    console.log(args);
   },
   {
-    urls: ["*://*.douyin.com/aweme/v1/web/aweme/listcollection/*"],
-    types: ["xmlhttprequest"]
+    urls: ["*://*.douyin.com/aweme/v1/web/aweme/listcollection/*"]
   }
 );
 console.log('======== Fetch Video for Douyin background end ========');
--- content/index.js
+++ content/index.js
@@ -1,3 +1,11 @@
 console.log('======== Fetch Video for Douyin start ========');
 console.log(document.querySelector('#douyin-right-container'));
 console.log('======== Fetch Video for Douyin load ========');
+
+// 监听来自 background 的消息
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'response-data') {
+    console.log('收到响应数据:', message.data);
+    // 在这里处理数据
+  }
+});
